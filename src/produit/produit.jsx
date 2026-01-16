fe747754095c16d9b616232f15398d958821075d
Ismailelbouzidi <ismailelbouzidi2@gmail.com>
2026-01-16 12:16:54 +0100

    Update produit.jsx

--- src/produit/produit.jsx
+++ src/produit/produit.jsx
@@ -1,10 +1,13 @@
-
 export default function Produit({produit}){
     return(
         <tr key={produit.id}>
-            <td>{produit.id}</td>
-            <td>{produit.nom}</td>
-            <td>{produit.prix} €</td>
+            <td className="align-middle">{produit.id}</td>
+            <td className="align-middle fw-semibold">{produit.nom}</td>
+            <td className="align-middle">
+                <span className="badge bg-success rounded-pill fs-6">
+                    {produit.prix} MAD
+                </span>
+            </td>
         </tr>
     )
 }
