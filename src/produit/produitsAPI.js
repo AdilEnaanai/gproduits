07cebccafbfdb9f003053ede9f111209c920a93a
[Abdelbarie Raissouni] <rabdlbari@gmail.com>
2026-01-16 11:01:17 +0100

    added the getbyid api

--- src/produit/produitsAPI.js
+++ src/produit/produitsAPI.js
@@ -1,25 +1,37 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 const port = 3000;
 let produits = [
-    { id: 1, nom: 'Produit A', prix: 10.0 },
-    { id: 2, nom: 'Produit B', prix: 20.0 },
+  { id: 1, nom: "Produit A", prix: 10.0 },
+  { id: 2, nom: "Produit B", prix: 20.0 },
 ];
 
-app.get('/produits', (req, res) => {
-    res.json(produits);
+app.get("/produits", (req, res) => {
+  res.json(produits);
 });
 
-app.post('/produits', (req, res) => {
-    const nouveauProduit = req.body;
-    produits.push(nouveauProduit);
-    res.status(201).json(nouveauProduit);
+app.get("/produits/:id", (req, res) => {
+  const id = req.params.id;
+
+  const produit = produits.find((p) => p.id == id);
+
+  if (!produit) {
+    return res.status(404).json({ message: "Produit not found" });
+  }
+
+  res.json(produit);
+});
+
+app.post("/produits", (req, res) => {
+  const nouveauProduit = req.body;
+  produits.push(nouveauProduit);
+  res.status(201).json(nouveauProduit);
 });
 
 app.listen(port, () => {
-    console.log(`Produits API listening at http://localhost:${port}`);
+  console.log(`Produits API listening at http://localhost:${port}`);
 });
